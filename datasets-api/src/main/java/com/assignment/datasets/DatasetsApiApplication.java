507cd456f9d1a0fa706287087d6d5e8622e2ea32
sakirhussain <sakirhussainpa@gmail.com>
2025-07-09 16:56:48 +0530

    missed init

---
+++ datasets-api/src/main/java/com/assignment/datasets/DatasetsApiApplication.java
@@ -0,0 +1,12 @@
+package com.assignment.datasets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatasetsApiApplication.class, args);
+  }
+}
---
+++ datasets-api/src/test/java/com/assignment/datasets/DatasetsApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.assignment.datasets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class DatasetsApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
